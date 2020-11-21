e790b7b4e54bff8d74145b997438050ce8571108
SkullOverHaul <72302535+SkullOverHaul@users.noreply.github.com>
2020-11-21 12:55:31 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,24 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+    var Options ={
+    bodyA:bodyA,
+    pointB:pointB,
+    length:40,
+    stiffness:0.5
+    }
+    this.pointB=pointB
+    this.sling= Constraint.create(Options)
+    World.add(world,this.sling)
+    }
+
+    fly(){
+    this.sling.bodyA=null
+    }
+    display(){
+    if(this.sling.bodyA){
+    var pointA=this.sling.bodyA.position
+    var pointB=this.pointB
+    line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+    }
+}
